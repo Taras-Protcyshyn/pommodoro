aec97fceacf93dda68ea9f1966fddc482cfee875
Taras Protsyshyn <taras@loungelogic.tv>
2020-03-24 17:48:48 +0200

    Add Timer component

---
+++ src/components/timer/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from 'antd'
+import moment from 'moment'
+
+const { Title, Paragraph, Text } = Typography;
+
+export const Timer = () => {
+  return (
+    <Typography>
+      <Title>Pommodoro app</Title>
+    </Typography>
+  )
+}
